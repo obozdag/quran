7f779300a0511a4207365e4653ae58ef61b2236c
obozdag <obozdag@gmail.com>
2020-04-17 04:41:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -43,6 +43,6 @@ self.addEventListener('fetch', evt => {
 	evt.respondWith(
 		caches.match(evt.request).then(
 			cacheResponse => {
-				return fetch(evt.request) || cacheResponse;
+				return cacheResponse || fetch(evt.request);
 		}));
 });
