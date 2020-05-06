0f9891151f95ff4819cf9d04eab5db22832696d2
obozdag <obozdag@gmail.com>
2020-05-06 11:02:40 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 if('serviceWorker' in navigator)
 {
-	navigator.serviceWorker.register('/sw.js').then(reg => {
-		console.log('serviceWorker registered. Scope: ', reg.scope);
+	navigator.serviceWorker.register('sw.js').then(reg => {
 	}).catch(err => {
-		console.log('serviceWorker not registered', err);
 	});
 }
