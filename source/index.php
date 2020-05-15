06e149d3b6edaa565a39397536327c64aae6b453
obozdag <obozdag@gmail.com>
2020-05-15 05:22:15 +0200

    Add files via upload

--- source/index.php
+++ source/index.php
@@ -6,17 +6,26 @@
 	<meta charset="utf-8">
 	<meta name="description" content="Easy Quran, easy to read, easy to scroll (top-to-bottom), lightweight (2.7MB), lightning fast, multi language, responsive, progressive web app.">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="apple-mobile-web-app-status-bar" content="#008b8b">
-	<meta name="theme-color" content="#008b8b">
-	<link rel="stylesheet" type="text/css" href="css/quran.css">
-	<link rel="apple-touch-icon" href="css/icons/quran_96x96.png">
-	<link rel="manifest" href="quran.json">
+	<meta name="apple-mobile-web-app-status-bar" content="<?= $color ?>">
+	<meta name="theme-color" content="<?= $color ?>">
+	<link rel="preload" href="css/fonts/hamdullah.ttf" as="font" crossorigin>
+	<link rel="preload" href="css/fonts/rb_icons.ttf" as="font" crossorigin>
+	<link rel="stylesheet" type="text/css" href="css/easy_quran.css">
+	<link rel="apple-touch-icon" href="css/icons/easy_quran_96x96.png">
+	<link rel="manifest" href="easy_quran.json">
 </head>
 <body>
 	<div id="loading_overlay">
 		<div id="loading_content">
 			<h3>Easy Quran</h3>
-			<p><img id="loading_image" src="css/icons/quran_loading.gif"></p>
+			<div class="loader">
+				<svg version="1.1"  x="0px" y="0px" width="100px" height="100px" viewBox="0 0 50 50" >
+					<path opacity="0.2" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z" />
+					<path  d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z">
+						<animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/>
+					</path>
+				</svg>
+			</div>
 			<p>Loading...</p>
 			<p>quran.fklavye.net</p>
 		</div>
@@ -35,15 +44,12 @@
 			<div class="row">
 				<label id="sura_list_label"></label>
 				<select id="sura_list">
-					<option></option>
 					<?php
 					// foreach ($rows_sura as $row):
 					// 	$id     = $row['id'];
 					// 	$sajdah = $row['sajdah'] ? '*': '';
 					// 	$option = $row['en'].' '.$id.' ('.$row['verses'].')'.$sajdah;
-					?>
-					<!-- <option value="<?= 's'.$id ?>"><?= $option ?></option> -->
-					<?php
+					// 	echo "<option value=\"s{$id}\">{$option}</option>";
 					// endforeach;
 					?>
 				</select>
@@ -72,6 +78,7 @@
 	</nav>
 	<nav id="nav_right" class="nav-side">
 		<i class="rb close_btn left" id="close_nav_right">c</i>
+		<h4 id="settings_header"></h4>
 		<div class="settings">
 			<div class="row">
 				<label id="font_family_list_label"></label>
@@ -98,66 +105,70 @@
 				<button type="button" class="btn btn_nav" id="reset_btn"></button>
 			</div>
 			<div id="nav_loading" class="nav-loading">
-				<img src="css/icons/loading.gif">
+				<p id="settings_message"></p>
 			</div>
 		</div>
 	</nav>
 	<div class="container" id="quran_container">
 		<div id="quran_verses" class="arabic">
-			<div class="page">
-				<?php
-				foreach($rows_verse as $row):
-					if ($row['page'] !== null):
-						$page_anchor_label = 's '.$row['page'];
-						$page_anchor_href  = 'p'.$row['page'];
-						$page_info         = $row['sura_id'].' '.$row['sura_tr'].' '.'['.$row['juz'].'. cz '.$row['hizb'].'. hzb '.$row['hizb_page'].'. syf'.']';
-						?>
-					</div>
-					<div class="page">
-						<p class="pip">
-							<?php if($row['new_juz']):
-								$juz_anchor_label = 'Cüz '.$row['new_juz'];
-								$juz_anchor_href  = 'j'.$row['new_juz'];
-								?>
-								<i class="ca" id="<?= $juz_anchor_href ?>"><?= $juz_anchor_label ?></i>
-							<?php endif; ?>
-							<i class="pa" id="<?= $page_anchor_href ?>" ><?= $page_anchor_label ?></i>
-							<i class="ib rb">i</i>
-							<i class="pi"><?= $page_info ?></i>
-						</p>
-						<?php
-					endif; ?>
-					<?php
-					if ($row['new_sura']):
-						$sura_href   = 's'.$row['sura_id'];
-						$sura_header = $row['sura_id'].' '.$row['sura_tr'].' Sûresi سُورَةُ '.$row['sura_name'];
-						?>
-						<h4 class="sn" id="<?= $sura_href ?>"><?= $sura_header ?></h4>
-						<?php if ($row['basmala']): ?>
-							<p class="basmala">بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ</p>
-						<?php endif;
-					endif; ?>
-					<?php
-					$sajdah_class = $row['sajdah'] ? ' class="sajdah"' : '';
+			<?php
+			foreach($rows_verse as $row):
+				$page        = $row['page'];
+				$sura_sajdah = $row['sura_sajdah'] ? '*': '';
+				$sura_info   = ' ('.$row['verses'].')'.$sura_sajdah;
+
+				if ($page !== null):
+					$page_anchor_label = 's '.$page;
+					$page_anchor_href  = 'p'.$page;
+					$page_info         = $row['sura_id'].' '.$row['sura_tr'].' '.$sura_info.' ['.$row['juz'].'. cz '.$row['hizb'].'. hzb '.$row['hizb_page'].'. syf'.']';
 					?>
-					<i class="vn">(<?= $row['verse_no'] ?>)</i><i<?= $sajdah_class ?>><?= $row['verse'] ?></i>
+					<?php if ($page > 0): ?>
+					</div>
+					<?php endif; ?>
+				<div class="page">
+					<p class="pip">
+						<?php if($row['new_juz']):
+							$juz_anchor_label = 'Cüz '.$row['new_juz'];
+							$juz_anchor_href  = 'j'.$row['new_juz'];
+							?>
+							<i class="ca" id="<?= $juz_anchor_href ?>"><?= $juz_anchor_label ?></i>
+						<?php endif; ?>
+						<i class="pa" id="<?= $page_anchor_href ?>" ><?= $page_anchor_label ?></i>
+						<i class="ib rb">i</i>
+						<i class="pi"><?= $page_info ?></i>
+					</p>
 					<?php
-				endforeach; ?>
+				endif; ?>
+				<?php
+				if ($row['new_sura']):
+					$sura_href   = 's'.$row['sura_id'];
+					$sura_header = $row['sura_id'].' '.$row['sura_tr'].$sura_info.' سُورَةُ '.$row['sura_name'];
+					?>
+					<h4 class="sn" id="<?= $sura_href ?>"><?= $sura_header ?></h4>
+					<?php if ($row['basmala']): ?>
+						<p class="basmala">بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ</p>
+					<?php endif;
+				endif; ?>
+				<?php
+				$sajdah_class = $row['sajdah'] ? ' class="sajdah"' : ''; ?>
+				<i class="vn">(<?= $row['verse_no'] ?>)</i><i<?= $sajdah_class ?>><?= $row['verse'] ?></i>
+				<?php
+			endforeach; ?>
+		</div>
+	</div>
+	<footer><a target="_blank" href="https://github.com/obozdag/quran"><i class="rb logo" title="Easy Quran">a</i> Easy Quran</a></footer>
+	<div class="overlay" id="program_info_popup">
+		<div class="popup">
+			<i id="close_popup_btn" class="rb close_btn right">c</i>
+			<h3><i class="rb logo">a</i> Easy Quran <?= $version ?></h3>
+			<div id="program_info_content">
 			</div>
 		</div>
-		<footer><a target="_blank" href="https://github.com/obozdag/quran"><i class="rb logo" title="Easy Quran">a</i> Easy Quran</a></footer>
-		<div class="overlay" id="program_info_popup">
-			<div class="popup">
-				<i id="close_popup_btn" class="rb close_btn right">c</i>
-				<h3><i class="rb logo">a</i> Easy Quran <?= $version ?></h3>
-				<div id="program_info_content">
-				</div>
-			</div>
-		</div>
-		<script src="js/swipe.js"></script>
-		<script src="js/lang.js"></script>
-		<script src="js/settings.js"></script>
-		<script src="js/quran.js"></script>
-		<script src="app.js"></script>
-	</body>
-	</html>
+	</div>
+	<script src="js/swipe.js"></script>
+	<script src="js/lang.js"></script>
+	<script src="js/settings.js"></script>
+	<script src="js/easy_quran.js"></script>
+	<script src="app.js"></script>
+</body>
+</html>
