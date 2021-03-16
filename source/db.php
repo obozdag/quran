049e6edeb64bd0edbec471772d22d653423f3b18
obozdag <obozdag@gmail.com>
2021-03-16 14:28:42 +0100

    Update db.php

--- source/db.php
+++ source/db.php
@@ -8,16 +8,17 @@ if(!$conn)
 mysqli_set_charset($conn, 'utf8');
 
 //Get Suras
-$sql_sura     = "SELECT * FROM fkl_sura ORDER BY tr";
+$sql_sura     = "SELECT * FROM fkl_sura";
 $result_sura  = mysqli_query($conn, $sql_sura);
 $rows_sura    = mysqli_fetch_all($result_sura, MYSQLI_ASSOC);
 
 //Get Verses of the Sura
-$sql_verse    = "SELECT v.*, s.name AS sura_name, s.tr AS sura_tr, s.en AS sura_en, s.basmala FROM fkl_verse v INNER JOIN fkl_sura s ON s.id = v.sura_id";
+$sql_verse    = "SELECT v.*, s.name AS sura_name, s.tr AS sura_tr, s.en AS sura_en, s.basmala, s.verses, s.sajdah AS sura_sajdah FROM fkl_verse v INNER JOIN fkl_sura s ON s.id = v.sura_id";
 $result_verse = mysqli_query($conn, $sql_verse);
 $rows_verse   = mysqli_fetch_all($result_verse, MYSQLI_ASSOC);
 
 mysqli_free_result($result_verse);
 mysqli_close($conn);
 
-$version = 'v1.9';
+$version = 'v1.92';
+$color   = '#008b8b';
